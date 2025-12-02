a84ca66d7d0f0a4bf270e832a39ba243caf8d4f0
knightchaser <agerio100@naver.com>
2025-12-02 17:15:40 +0900

    Docs: Add documentation on data.h

--- src/data.h
+++ src/data.h
@@ -6,8 +6,13 @@
 
 #include <stdio.h> // Just for FILE
 
+// Current Line number
 extern_ int Line;
+// Character put back by scanner for re-reading
 extern_ int Putback;
+// Input file (source code)
 extern_ FILE *Infile;
+// Output file (generated code, currently Assembly)
 extern_ FILE *Outfile;
+// Latest token scanned
 extern_ struct token Token;
